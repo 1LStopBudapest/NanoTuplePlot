d9820ffdb5dd2a6d71be05dd839ba096f8ed8511
Koushik <kmandalforstudy@gmail.com>
2020-07-14 13:27:12 +0200

    bug fixed

--- StackPlotScript.py
+++ StackPlotScript.py
@@ -5,24 +5,33 @@ import types
 sys.path.append('../')
 from Sample.SampleChain import SampleChain
 from Sample.Dir import plotDir
+from Sample.FileList_2016 import samples as samples_2016
 
 samplesRun = ['WJetsToLNu', 'TTSingleLep_pow', 'MET_Data',]
 fileperjobMC = 1 
 fileperjobData = 1
 TotJobs = 4
+year = 2016
 
 txtline = []
 
+if year==2016:
+    samplelist = samples_2016
+elif year==2017:
+    samplelist = samples_2017
+else:
+    samplelist = samples_2018
+
 for sL in samplesRun:
-    if isinstance(SampleChain.samplelist[sL][0], types.ListType):
-        for s in SampleChain.samplelist[sL]:
-            sample = list(SampleChain.samplelist.keys())[list(SampleChain.samplelist.values()).index(s)]
+    if isinstance(samplelist[sL][0], types.ListType):
+        for s in samplelist[sL]:
+            sample = list(samplelist.keys())[list(samplelist.values()).index(s)]
             fileperjob = fileperjobData if ('Run' in sample or 'Data' in sample) else fileperjobMC
-            tfiles = len(SampleChain.getfilelist(SampleChain.samplelist[sample][0]))
+            tfiles = len(SampleChain.getfilelist(samplelist[sample][0]))
             for i in range(0, tfiles, fileperjobMC):
                 txtline.append("python StackHistMaker.py --sample %s --startfile %i --nfiles %i\n"%(sample, i, fileperjobMC))
     else:
-        tfiles = len(SampleChain.getfilelist(SampleChain.samplelist[sL][0]))
+        tfiles = len(SampleChain.getfilelist(samplelist[sL][0]))
         fileperjob = fileperjobData if ('Run' in sL or 'Data' in sL) else fileperjobMC
         for i in range(0, tfiles, fileperjobMC):
             txtline.append("python StackHistMaker.py --sample %s --startfile %i --nfiles %i\n"%(sL, i, fileperjobMC))
@@ -39,7 +48,11 @@ bashline = []
 bashline.append('parallel --jobs %i < parallelJobsubmit.txt\n'%TotJobs)
 
 for sL in samplesRun:
-    bashline.append('hadd StackHist_%s.root StackHist_%s_*.root\n'%(sL, sL))
+    if 'Data' in sL:
+        SLi = sL.replace('Data','')+'Run'
+        bashline.append('hadd StackHist_%s.root StackHist_%s*.root\n'%(sL, sLi))
+    else:
+        bashline.append('hadd StackHist_%s.root StackHist_%s_*.root\n'%(sL, sL))
     bashline.append('mv StackHist_%s.root %s\n'%(sL, Rootfilesdirpath))
 
 l = str(" ".join(s for s in samplesRun))
