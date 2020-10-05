45750a3f353b7d7e5bd987123b2a62aabc140481
mahmoud moussa <mahmoud.gadallah@cern.ch>
2020-10-05 20:13:43 +0200

    change Jet_ to JetGood_ in varhandler.py but in fillhistos.py did not change GenJet_pt into GenJetGood_pt there is no such var in the tree

--- VarHandler.py
+++ VarHandler.py
@@ -31,8 +31,8 @@ class VarHandler():
 
     def dphicut(self):
         cut = False
-        if len(self.selectjetIdx(30)) >=2 and self.tr.Jet_pt[self.selectjetIdx(30)[1]]> 60:
-            if DeltaPhi(self.tr.Jet_phi[self.selectjetIdx(30)[0]], self.tr.Jet_phi[self.selectjetIdx(30)[1]])<2.5:
+        if len(self.selectjetIdx(30)) >=2 and self.tr.JetGood_pt[self.selectjetIdx(30)[1]]> 60:
+            if DeltaPhi(self.tr.JetGood_phi[self.selectjetIdx(30)[0]], self.tr.JetGood_phi[self.selectjetIdx(30)[1]])<2.5:
                 cut = True
         return cut
 
@@ -49,7 +49,7 @@ class VarHandler():
 
     def XtraJetVeto(self):
         cut = True
-        if len(self.selectjetIdx(30)) >=3 and self.tr.Jet_pt[self.selectjetIdx(30)[2]]> 60:
+        if len(self.selectjetIdx(30)) >=3 and self.tr.JetGood_pt[self.selectjetIdx(30)[2]]> 60:
             cut = False
         return cut
 
@@ -69,14 +69,14 @@ class VarHandler():
     def calHT(self):
         HT = 0
         for i in self.selectjetIdx(30):
-            HT = HT + self.tr.Jet_pt[i]
+            HT = HT + self.tr.JetGood_pt[i]
         return HT
 
     def calNj(self, thrsld):
         return len(self.selectjetIdx(thrsld))
         
     def getISRPt(self):
-        return self.tr.Jet_pt[self.selectISRjetIdx()[0]] if len(self.selectISRjetIdx()) else 0
+        return self.tr.JetGood_pt[self.selectISRjetIdx()[0]] if len(self.selectISRjetIdx()) else 0
     
     def cntBtagjet(self, discOpt='DeepCSV', ptthrsld=20):
         return len(self.selectBjetIdx(discOpt, ptthrsld))
@@ -89,23 +89,23 @@ class VarHandler():
     
     def selectjetIdx(self, thrsld):
         idx = []
-        for i in range(len(self.tr.Jet_pt)):
-            if self.tr.Jet_pt[i]>thrsld and abs(self.tr.Jet_eta[i])<2.4:
+        for i in range(len(self.tr.JetGood_pt)):
+            if self.tr.JetGood_pt[i]>thrsld and abs(self.tr.JetGood_eta[i])<2.4:
                 idx.append(i)
         return idx
 
     def selectISRjetIdx(self, thrsld=100):
         idx = []
-        for i in range(len(self.tr.Jet_pt)):
-            if self.tr.Jet_pt[i]>thrsld and abs(self.tr.Jet_eta[i])<2.4:
+        for i in range(len(self.tr.JetGood_pt)):
+            if self.tr.JetGood_pt[i]>thrsld and abs(self.tr.JetGood_eta[i])<2.4:
                 idx.append(i)
         return idx
 
     def selectBjetIdx(self, discOpt='DeepCSV', ptthrsld=20):
         idx = []
-        for i in range(len(self.tr.Jet_pt)):
-            if self.tr.Jet_pt[i]>ptthrsld and abs(self.tr.Jet_eta[i])<2.4:
-                if (self.isBtagCSVv2(self.tr.Jet_btagCSVV2[i], self.yr) if discOpt == 'CSVV2' else self.isBtagDeepCSV(self.tr.Jet_btagDeepB[i], self.yr)):
+        for i in range(len(self.tr.JetGood_pt)):
+            if self.tr.JetGood_pt[i]>ptthrsld and abs(self.tr.JetGood_eta[i])<2.4:
+                if (self.isBtagCSVv2(self.tr.JetGood_btagCSVV2[i], self.yr) if discOpt == 'CSVV2' else self.isBtagDeepCSV(self.tr.JetGood_btagDeepB[i], self.yr)):
                     idx.append(i)
         return idx
 
@@ -318,6 +318,6 @@ class VarHandler():
     def selectGenjetIdx(self, flavor = 5):
         idx = []
         for i in range(self.tr.nGenJet):
-            if abs(self.tr.GenJet_partonFlavour[i])==flavor:
+            if abs(self.tr.GenJetGood_partonFlavour[i])==flavor:
                 idx.append(i)
         return idx
