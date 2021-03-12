4ffb13c98086f14710f73ba4f161f768a280ee9e
RN <rabnora@example.com>
2021-03-12 23:30:58 +0100

    added T2tt signal-handeling to StackPlotScript.py and StackHistMaker.py

--- StackPlotScript.py
+++ StackPlotScript.py
@@ -23,18 +23,21 @@ else:
     samplelist = samples_2018
 
 for sL in samplesRun:
-    if isinstance(samplelist[sL][0], types.ListType):
-        for s in samplelist[sL]:
-            sample = list(samplelist.keys())[list(samplelist.values()).index(s)]
-            fileperjob = fileperjobData if ('Run' in sample or 'Data' in sample) else fileperjobMC
-            tfiles = len(SampleChain.getfilelist(samplelist[sample][0]))
-            for i in range(0, tfiles, fileperjobMC):
-                txtline.append("python StackHistMaker.py --sample %s --startfile %i --nfiles %i\n"%(sample, i, fileperjobMC))
+    if 'T2tt' in sL:
+        txtline.append("python StackHistMaker.py --sample %s --startfile %i --nfiles %i\n"%(sL, 1, fileperjobMC))
     else:
-        tfiles = len(SampleChain.getfilelist(samplelist[sL][0]))
-        fileperjob = fileperjobData if ('Run' in sL or 'Data' in sL) else fileperjobMC
-        for i in range(0, tfiles, fileperjobMC):
-            txtline.append("python StackHistMaker.py --sample %s --startfile %i --nfiles %i\n"%(sL, i, fileperjobMC))
+        if isinstance(samplelist[sL][0], types.ListType):
+            for s in samplelist[sL]:
+                sample = list(samplelist.keys())[list(samplelist.values()).index(s)]
+                fileperjob = fileperjobData if ('Run' in sample or 'Data' in sample) else fileperjobMC
+                tfiles = len(SampleChain.getfilelist(samplelist[sample][0]))
+                for i in range(0, tfiles, fileperjobMC):
+                    txtline.append("python StackHistMaker.py --sample %s --startfile %i --nfiles %i\n"%(sample, i, fileperjobMC))
+        else:
+            tfiles = len(SampleChain.getfilelist(samplelist[sL][0]))
+            fileperjob = fileperjobData if ('Run' in sL or 'Data' in sL) else fileperjobMC
+            for i in range(0, tfiles, fileperjobMC):
+                txtline.append("python StackHistMaker.py --sample %s --startfile %i --nfiles %i\n"%(sL, i, fileperjobMC))
                 
 fout = open("parallelJobsubmit.txt", "w")
 fout.write(''.join(txtline))
@@ -51,6 +54,8 @@ for sL in samplesRun:
     if 'Data' in sL:
         sLi = sL.replace('Data','')+'Run'
         bashline.append('hadd StackHist_%s.root StackHist_%s*.root\n'%(sL, sLi))
+    elif 'T2tt' in sL:
+        bashline.append('hadd StackHist_%s.root StackHist_%s*.root\n'%(sL, sL))
     elif isinstance(samplelist[sL][0], types.ListType):
         sLi = 'hadd StackHist_'+sL+'.root'+str("".join(' StackHist_'+list(samplelist.keys())[list(samplelist.values()).index(s)]+'*.root' for s in samplelist[sL]))
         bashline.append('%s\n'%sLi)
