63c0abefde4799d5c209e43907f0aa289b31b7af
RN <rabnora@example.com>
2021-11-08 10:41:16 +0100

    correct plotting

--- 1DPlot_IVF.py
+++ 1DPlot_IVF.py
@@ -10,6 +10,7 @@ from Sample.Dir import plotDir
 from Helper.HistInfo import HistInfo
 from Helper.PlotHelper import *
 from Sample.FileList_2016 import samples as samples_2016
+from Helper.VarCalc import *
 
 
 def get_parser():
@@ -22,7 +23,6 @@ def get_parser():
     argParser.add_argument('--startfile',        action='store',                     type=int,            default=0,                                                help="start from which root file like 0th or 10th etc?" )
     argParser.add_argument('--nfiles',           action='store',                     type=int,            default=-1,                                               help="No of files to run. -1 means all files" )
     argParser.add_argument('--nevents',           action='store',                    type=int,            default=-1,                                               help="No of events to run. -1 means all events" )
-    
 
     return argParser
 
@@ -30,7 +30,9 @@ options = get_parser().parse_args()
 
 samples  = options.sample
 year = options.year
+nEvents = options.nevents
 
+isData = True if ('Run' in samples or 'Data' in samples) else False
 DataLumi=1.0
 
 if year==2016:
@@ -52,64 +54,35 @@ if not os.path.exists(Rootfilesdirpath):
     os.makedirs(Rootfilesdirpath)
 
 histext = samples
+for l in list(samplelist.values()):
+    if samplelist[samples] in l: histext = list(samplelist.keys())[list(samplelist.values()).index(l)]
+sample = samples
+print 'running over: ', sample
+hfile = ROOT.TFile('1DHist_'+sample+'_%i_%i'%(options.startfile+1, options.startfile + options.nfiles)+'.root', 'RECREATE')
+histos = {}
+histos['nSV'] = HistInfo(hname = 'nSV', sample = histext, binning=[15,0,15], histclass = ROOT.TH1F).make_hist()
+histos['Ntracks'] = HistInfo(hname = 'Ntracks', sample = histext, binning=[20,0,20], histclass = ROOT.TH1F).make_hist()
+histos['SVdxy'] = HistInfo(hname = 'SVdxy', sample = histext, binning=[50,0,5], histclass = ROOT.TH1F).make_hist()
+histos['SVdxySig'] = HistInfo(hname = 'SVdxySig', sample = histext, binning=[50,0,50], histclass = ROOT.TH1F).make_hist()
+histos['SVmass'] = HistInfo(hname = 'SVmass', sample = histext, binning=[20,0,10], histclass = ROOT.TH1F).make_hist()
+histos['SVdlenSig'] = HistInfo(hname = 'SVdlenSig', sample = histext, binning=[50,0,50], histclass = ROOT.TH1F).make_hist()
+histos['SVpAngle'] = HistInfo(hname = 'SVpAngle', sample = histext, binning=[50,0.5,3.5], histclass = ROOT.TH1F).make_hist()
+histos['SVpT'] = HistInfo(hname = 'SVpT', sample = histext, binning=[50,0,50], histclass = ROOT.TH1F).make_hist()
+histos['SVdR'] = HistInfo(hname = 'SVdR', sample = histext, binning=[40,0,4], histclass = ROOT.TH1F).make_hist()
 
-if isinstance(samplelist[samples][0], types.ListType):
-    for s in samplelist[samples]:
-        sample = list(samplelist.keys())[list(samplelist.values()).index(s)]
-        print 'running over: ', sample
-        hfile = ROOT.TFile('1DHist_'+sample+'_%i_%i'%(options.startfile+1, options.startfile + options.nfiles)+'.root', 'RECREATE')
-        histos = {}
-        histos['nSV'] = HistInfo(hname = 'nSV', sample = histext, binning=[20,0,20], histclass = ROOT.TH1F).make_hist()
-        histos['Ntracks'] = HistInfo(hname = 'Ntracks', sample = histext, binning=[20,0,20], histclass = ROOT.TH1F).make_hist()
-        histos['SVdxy'] = HistInfo(hname = 'SVdxy', sample = histext, binning=[40,0,40], histclass = ROOT.TH1F).make_hist()
-        histos['SVdxySig'] = HistInfo(hname = 'SVdxySig', sample = histext, binning=[100,0,2500], histclass = ROOT.TH1F).make_hist()
-        histos['SVmass'] = HistInfo(hname = 'SVmass', sample = histext, binning=[40,0,20], histclass = ROOT.TH1F).make_hist()
-        histos['SVdlenSig'] = HistInfo(hname = 'SVdlenSig', sample = histext, binning=[100,0,2500], histclass = ROOT.TH1F).make_hist()
-        histos['SVpAngle'] = HistInfo(hname = 'SVpAngle', sample = histext, binning=[40,0,4], histclass = ROOT.TH1F).make_hist()
-        histos['SVpT'] = HistInfo(hname = 'SVpT', sample = histext, binning=[100,0,250], histclass = ROOT.TH1F).make_hist()
-        histos['SVdR'] = HistInfo(hname = 'SVdR', sample = histext, binning=[40,0,4], histclass = ROOT.TH1F).make_hist()
+ch = SampleChain(sample, options.startfile, options.nfiles, year).getchain()
+print 'Total events of selected files of the', sample, 'sample: ', ch.GetEntries()
+n_entries = ch.GetEntries()
+nevtcut = n_entries -1 if nEvents == - 1 else nEvents - 1
+print 'Running over total events: ', nevtcut+1
 
-        ch = SampleChain(sample, options.startfile, options.nfiles, year).getchain()
-        print 'Total events of selected files of the', sample, 'sample: ', ch.GetEntries()
-        FillHistos_IVF(histos, ch, options.year, options.nevents, sample, DataLumi, False).fill()
-        hfile.Write()
-
-else:
-    for l in list(samplelist.values()):
-        if samplelist[samples] in l: histext = list(samplelist.keys())[list(samplelist.values()).index(l)]
-    sample = samples
-    print 'running over: ', sample
-    hfile = ROOT.TFile('1DHist_'+sample+'_%i_%i'%(options.startfile+1, options.startfile + options.nfiles)+'.root', 'RECREATE')
-    histos = {}
-    histos['nSV'] = HistInfo(hname = 'nSV', sample = histext, binning=[20,0,20], histclass = ROOT.TH1F).make_hist()
-    histos['Ntracks'] = HistInfo(hname = 'Ntracks', sample = histext, binning=[20,0,20], histclass = ROOT.TH1F).make_hist()
-    histos['SVdxy'] = HistInfo(hname = 'SVdxy', sample = histext, binning=[40,0,40], histclass = ROOT.TH1F).make_hist()
-    histos['SVdxySig'] = HistInfo(hname = 'SVdxySig', sample = histext, binning=[100,0,2500], histclass = ROOT.TH1F).make_hist()
-    histos['SVmass'] = HistInfo(hname = 'SVmass', sample = histext, binning=[40,0,20], histclass = ROOT.TH1F).make_hist()
-    histos['SVdlenSig'] = HistInfo(hname = 'SVdlenSig', sample = histext, binning=[100,0,2500], histclass = ROOT.TH1F).make_hist()
-    histos['SVpAngle'] = HistInfo(hname = 'SVpAngle', sample = histext, binning=[40,0,4], histclass = ROOT.TH1F).make_hist()
-    histos['SVpT'] = HistInfo(hname = 'SVpT', sample = histext, binning=[100,0,250], histclass = ROOT.TH1F).make_hist()
-    histos['SVdR'] = HistInfo(hname = 'SVdR', sample = histext, binning=[40,0,4], histclass = ROOT.TH1F).make_hist()
-
-    ch = SampleChain(sample, options.startfile, options.nfiles, options.year).getchain()
-    print 'Total events of selected files of the', sample, 'sample: ', ch.GetEntries()
-    FillHistos_IVF(histos, ch, options.year, options.nevents, sample, DataLumi, False).fill()
-    hfile.Write()
-
-#outputDir = os.getcwd()
+ch = SampleChain(sample, options.startfile, options.nfiles, options.year).getchain()
+print 'Total events of selected files of the', sample, 'sample: ', ch.GetEntries()
+FillHistos_IVF(histos, ch, options.year, options.nevents, sample, DataLumi, False).fill()
+hfile.Write()
 
 bashline = []    
-
-if 'Data' in samples:
-    sLi = samples.replace('Data','')+'Run'
-    bashline.append('hadd 1DHist_%s.root 1DHist_%s*.root\n'%(samples, sLi))
-elif 'T2tt' in samples:
-    bashline.append('hadd 1DHist_%s.root 1DHist_%s_*.root\n'%(samples, samples))
-elif isinstance(samplelist[samples][0], types.ListType):
-    sLi = 'hadd 1DHist_'+samples+'.root'+str("".join(' 1DHist_'+list(samplelist.keys())[list(samplelist.values()).index(s)]+'*.root' for s in samplelist[samples]))
-    bashline.append('%s\n'%sLi)
-else:
-    bashline.append('hadd 1DHist_%s.root 1DHist_%s_*.root\n'%(samples, samples))
+bashline.append('hadd 1DHist_%s.root 1DHist_%s_*.root\n'%(samples, samples))
 bashline.append('mv 1DHist_%s.root %s\n'%(samples, Rootfilesdirpath))
 
 
