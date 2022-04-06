3463b4f093908f066f64e3657072761ee7b903e2
RN <rabnora@example.com>
2022-04-06 09:40:05 +0200

    full working version

---
+++ TrueFill.py
@@ -0,0 +1,96 @@
+import ROOT
+import types
+import os, sys
+
+sys.path.append('../')
+from Helper.VarCalc import *
+from Helper.TreeVarSel_true import TreeVarSel
+from Helper.MCWeight import MCWeight
+from Helper.HistInfo import HistInfo
+from Helper.PlotHelper import *
+from Sample.SampleChain import SampleChain
+from Sample.Dir import plotDir
+
+class TrueFill():
+
+    def __init__(self, histos, chain, year, nEvents, sample, DataLumi=1.0):
+        self.histos = histos
+        self.chain = chain
+        self.year = year
+        self.nEvents = nEvents
+        self.sample = sample
+        self.DataLumi = DataLumi
+
+        keylist = ['gStop_dx', 'gStop_dy', 'gStop_dz', 'gVtx_dx', 'gVtx_dy', 'gVtx_dz',
+                   'gStop_gVtx_dx', 'gStop_gVtx_dy', 'gStop_gVtx_dz', 'gStop_gAStop_dx', 'gStop_gAStop_dy', 'gStop_gAStop_dz',
+                   'gLSP_gStop_dx', 'gLSP_gStop_dy', 'gLSP_gStop_dz', 'PV_gVtx_dx', 'PV_gVtx_dy', 'PV_gVtx_dz',
+                   'PV_gLSP_dx', 'PV_gLSP_dy', 'PV_gLSP_dz', 
+                   'gStop_gVtx_2D', 'gStop_gAStop_2D', 'gLSP_gStop_2D', 'PV_gVtx_2D', 'PV_gLSP_2D',
+                   'gStop_gVtx_3D', 'gStop_gAStop_3D', 'gLSP_gStop_3D', 'PV_gVtx_3D', 'PV_gLSP_3D']
+        self.vardic = {key: None for key in keylist}
+
+    def fill(self):
+        tr = self.chain
+        vardic = self.vardic
+        n_entries = tr.GetEntries()
+        nevtcut = n_entries -1 if self.nEvents == - 1 else self.nEvents - 1
+        print 'Running over total events: ', nevtcut+1
+
+        for ientry in range(n_entries):
+            if ientry > nevtcut: break
+            if ientry % (nevtcut/10)==0 : print 'processing ', ientry,'th event'
+            tr.GetEntry(ientry) #ientry = i. event 
+            getsel = TreeVarSel(tr, self.year)
+            var = {key: None for key in vardic} #reseting the var dictionary for each event
+            MCcorr = MCWeight(tr, self.year, self.sample).getTotalWeight()
+            lumiscale = 1.0
+
+            genStop = getsel.getGenPartStop()
+            genVtx = getsel.getGenVtx()
+            genAntiStop = getsel.getGenPartAntiStop()
+            genLSP = getsel.getLSP()
+            pv = getsel.getPV()
+
+            var['gStop_dx'] = getsel.getGenPartStop()[0]['x']*10 #mm
+            var['gStop_dy'] = getsel.getGenPartStop()[0]['y']*10
+            var['gStop_dz'] = getsel.getGenPartStop()[0]['z']    #cm
+            var['gVtx_dx'] = getsel.getGenVtx()['x']*10
+            var['gVtx_dy'] = getsel.getGenVtx()['y']*10
+            var['gVtx_dz'] = getsel.getGenVtx()['z']
+            var['gStop_gVtx_dx'] = getsel.distance(genVtx, genStop[0], 'x')*10000 # original is in cm, right?
+            var['gStop_gVtx_dy'] = getsel.distance(genVtx, genStop[0], 'y')*10000 # ==> um
+            var['gStop_gVtx_dz'] = getsel.distance(genVtx, genStop[0], 'z')*10000
+            var['gStop_gAStop_dx'] = getsel.distance(genAntiStop[0], genStop[0], 'x')*10000 
+            var['gStop_gAStop_dy'] = getsel.distance(genAntiStop[0], genStop[0], 'y')*10000 
+            var['gStop_gAStop_dz'] = getsel.distance(genAntiStop[0], genStop[0], 'z')*10000
+            var['PV_gVtx_dx'] = getsel.distance(genVtx, pv, 'x')*10000
+            var['PV_gVtx_dy'] = getsel.distance(genVtx, pv, 'y')*10000
+            var['PV_gVtx_dz'] = getsel.distance(genVtx, pv, 'z')*10000
+            var['gStop_gVtx_2D'] = sqrt(var['gStop_gVtx_dx']**2 + var['gStop_gVtx_dy']**2)
+            var['gStop_gAStop_2D'] = sqrt(var['gStop_gAStop_dx']**2 + var['gStop_gAStop_dy']**2)
+            var['PV_gVtx_2D'] = sqrt(var['PV_gVtx_dx']**2 + var['PV_gVtx_dy']**2)
+            var['gStop_gVtx_3D'] = sqrt(var['gStop_gVtx_dx']**2 + var['gStop_gVtx_dy']**2 + var['gStop_gVtx_dz']**2)
+            var['gStop_gAStop_3D'] = sqrt(var['gStop_gAStop_dx']**2 + var['gStop_gAStop_dy']**2 + var['gStop_gAStop_dz']**2)
+            var['PV_gVtx_3D'] = sqrt(var['PV_gVtx_dx']**2 + var['PV_gVtx_dy']**2 + var['PV_gVtx_dz']**2)
+            if len(genLSP) > 0:
+                var['gLSP_gStop_dx'] = getsel.distance(genLSP[0], genStop[0], 'x')
+                var['gLSP_gStop_dy'] = getsel.distance(genLSP[0], genStop[0], 'y') 
+                var['gLSP_gStop_dz'] = getsel.distance(genLSP[0], genStop[0], 'z')
+                var['PV_gLSP_dx'] = getsel.distance(genLSP[0], pv, 'x')
+                var['PV_gLSP_dy'] = getsel.distance(genLSP[0], pv, 'y')
+                var['PV_gLSP_dz'] = getsel.distance(genLSP[0], pv, 'z')
+                var['gLSP_gStop_2D'] = sqrt(var['gLSP_gStop_dx']**2 + var['gLSP_gStop_dy']**2)
+                var['PV_gLSP_2D'] = sqrt(var['PV_gLSP_dx']**2 + var['PV_gLSP_dy']**2)
+                var['gLSP_gStop_3D'] = sqrt(var['gLSP_gStop_dx']**2 + var['gLSP_gStop_dy']**2 + var['gLSP_gStop_dz']**2)
+                var['PV_gLSP_3D'] = sqrt(var['PV_gLSP_dx']**2 + var['PV_gLSP_dy']**2 + var['PV_gLSP_dz']**2)
+
+
+            for key in self.histos:
+                if key in var.keys():
+                    if var[key] is not None:
+                        if isinstance(var[key], types.ListType):
+                            for x in var[key]: Fill1D(self.histos[key], x, lumiscale * MCcorr)
+                        else:
+                            Fill1D(self.histos[key], var[key], lumiscale * MCcorr)
+                else:
+                    print "You are trying to fill the histos for the keys ", key, " which are missing in var dictionary"
---
+++ TruePlot.py
@@ -0,0 +1,93 @@
+import ROOT
+import types
+import os, sys
+
+from TrueFill import TrueFill
+
+sys.path.append('../')
+from Sample.SampleChain import SampleChain
+from Helper.VarCalc import *
+from Helper.HistInfo import HistInfo
+from Helper.PlotHelper import *
+from Sample.Dir import plotDir
+
+
+def get_parser():
+    ''' Argument parser.'''
+    import argparse
+    argParser = argparse.ArgumentParser(description = "Argument parser")
+    argParser.add_argument('--sample',           action='store',                     type=str,            default='UL17V9_Full99mm',                                help="Which sample?" )
+    argParser.add_argument('--year',             action='store',                     type=int,            default=2016,                                             help="Which year?" )
+    argParser.add_argument('--startfile',        action='store',                     type=int,            default=0,                                                help="start from which root file like 0th or 10th etc?" )
+    argParser.add_argument('--nfiles',           action='store',                     type=int,            default=-1,                                               help="No of files to run. -1 means all files" )
+    argParser.add_argument('--nevents',          action='store',                     type=int,            default=-1,                                               help="No of events to run. -1 means all events" )
+    
+    return argParser
+
+options = get_parser().parse_args()
+sample  = options.sample
+year = options.year
+
+Rootfilesdirpath = os.path.join(plotDir, "1DFiles/truce")
+if not os.path.exists(Rootfilesdirpath): 
+    os.makedirs(Rootfilesdirpath)
+
+print 'running over: ', sample
+hfile = ROOT.TFile('1DHist_'+sample+'_%i_%i'%(options.startfile+1, options.startfile + options.nfiles)+'.root', 'RECREATE')
+histos = {}
+#coordinates
+histos['gStop_dx'] = HistInfo(hname = 'gStop_dx', sample = sample, binning=[40,-0.3,-0.2], histclass = ROOT.TH1F).make_hist()
+histos['gStop_dy'] = HistInfo(hname = 'gStop_dy', sample = sample, binning=[40,0.65,0.75], histclass = ROOT.TH1F).make_hist()
+histos['gStop_dz'] = HistInfo(hname = 'gStop_dz', sample = sample, binning=[40,-15,15], histclass = ROOT.TH1F).make_hist()
+histos['gVtx_dx'] = HistInfo(hname = 'gVtx_dx', sample = sample, binning=[40,-0.3,-0.2], histclass = ROOT.TH1F).make_hist()
+histos['gVtx_dy'] = HistInfo(hname = 'gVtx_dy', sample = sample, binning=[40,0.65,0.75], histclass = ROOT.TH1F).make_hist()
+histos['gVtx_dz'] = HistInfo(hname = 'gVtx_dz', sample = sample, binning=[40,-15,15], histclass = ROOT.TH1F).make_hist()
+#distance
+histos['gStop_gVtx_dx'] = HistInfo(hname = 'gStop_gVtx_dx', sample = sample, binning=[40,0,1], histclass = ROOT.TH1F).make_hist()
+histos['gStop_gVtx_dy'] = HistInfo(hname = 'gStop_gVtx_dy', sample = sample, binning=[40,0,1], histclass = ROOT.TH1F).make_hist()
+histos['gStop_gVtx_dz'] = HistInfo(hname = 'gStop_gVtx_dz', sample = sample, binning=[40,0,40], histclass = ROOT.TH1F).make_hist()
+histos['gStop_gAStop_dx'] = HistInfo(hname = 'gStop_gAStop_dx', sample = sample, binning=[40,-1,1], histclass = ROOT.TH1F).make_hist()
+histos['gStop_gAStop_dy'] = HistInfo(hname = 'gStop_gAStop_dy', sample = sample, binning=[40,-1,1], histclass = ROOT.TH1F).make_hist()
+histos['gStop_gAStop_dz'] = HistInfo(hname = 'gStop_gAStop_dz', sample = sample, binning=[40,-1,1], histclass = ROOT.TH1F).make_hist()
+histos['gLSP_gStop_dx'] = HistInfo(hname = 'gLSP_gStop_dx', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['gLSP_gStop_dy'] = HistInfo(hname = 'gLSP_gStop_dy', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['gLSP_gStop_dz'] = HistInfo(hname = 'gLSP_gStop_dz', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gVtx_dx'] = HistInfo(hname = 'PV_gVtx_dx', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gVtx_dy'] = HistInfo(hname = 'PV_gVtx_dy', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gVtx_dz'] = HistInfo(hname = 'PV_gVtx_dz', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gLSP_dx'] = HistInfo(hname = 'PV_gLSP_dx', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gLSP_dy'] = HistInfo(hname = 'PV_gLSP_dy', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gLSP_dz'] = HistInfo(hname = 'PV_gLSP_dz', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+#2D
+histos['gStop_gVtx_2D'] = HistInfo(hname = 'gStop_gVtx_2D', sample = sample, binning=[40,0,1], histclass = ROOT.TH1F).make_hist()
+histos['gStop_gAStop_2D'] = HistInfo(hname = 'gStop_gAStop_2D', sample = sample, binning=[40,0,1], histclass = ROOT.TH1F).make_hist()
+histos['gLSP_gStop_2D'] = HistInfo(hname = 'gLSP_gStop_2D', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gVtx_2D'] = HistInfo(hname = 'PV_gVtx_2D', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gLSP_2D'] = HistInfo(hname = 'PV_gLSP_2D', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+#3D
+histos['gStop_gVtx_3D'] = HistInfo(hname = 'gStop_gVtx_3D', sample = sample, binning=[40,0,1], histclass = ROOT.TH1F).make_hist()
+histos['gStop_gAStop_3D'] = HistInfo(hname = 'gStop_gAStop_3D', sample = sample, binning=[40,0,1], histclass = ROOT.TH1F).make_hist()
+histos['gLSP_gStop_3D'] = HistInfo(hname = 'gLSP_gStop_3D', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gVtx_3D'] = HistInfo(hname = 'PV_gVtx_3D', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+histos['PV_gLSP_3D'] = HistInfo(hname = 'PV_gLSP_3D', sample = sample, binning=[40,0,400], histclass = ROOT.TH1F).make_hist()
+
+
+ch = SampleChain(sample, options.startfile, options.nfiles, options.year).getchain()
+print 'Total events of selected files of the', sample, 'sample: ', ch.GetEntries()
+TrueFill(histos, ch, options.year, options.nevents, sample).fill()
+
+hfile.Write()
+bashline = []    
+bashline.append('hadd 1DHist_%s.root 1DHist_%s_*.root\n'%(sample, sample))
+bashline.append('mv 1DHist_%s.root %s\n'%(sample, Rootfilesdirpath))
+
+fsh = open("FileHandle.sh", "w")
+fsh.write(''.join(bashline))
+fsh.close()
+os.system('chmod 744 FileHandle.sh')
+os.system('./FileHandle.sh')
+os.system('rm *.root FileHandle.sh')
+
+outputDir = plotDir
+for key in histos:
+    Plot1D(histos[key], outputDir, islogy=True)
