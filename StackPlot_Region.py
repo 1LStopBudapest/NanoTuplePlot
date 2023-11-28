5aafb4ff058cdc88f816a83caf8235ae9742b6c5
RN <rabnora@example.com>
2023-11-28 17:08:04 +0100

    changed default samplelist in StackPlot_Region

--- StackPlot_Region.py
+++ StackPlot_Region.py
@@ -18,7 +18,7 @@ def get_parser():
     nargs='+',                              # one or more parameters to this switch
     type=str,                               # /parameters/ are ints
     dest='alist',                           # store in 'list'.
-    default=['TTV', 'QCD', 'VV', 'DYJetsToLL', 'TTbar', 'ZJetsToNuNu', 'WJetsToLNu', 'ST', 'Sig_Displaced_300_290', 'Sig_Displaced_350_335', 'Sig_Displaced_400_380'],      # last sample should be data as to be consistent with StackHists funtion.
+    default=['VV', 'DYJetsToLL', 'ST', 'QCD', 'WJetsToLNu', 'TTbar', 'TTV', 'ZJetsToNuNu', 'T2tt_500_470', 'T2tt_800_720'], # last sample should be data as to be consistent with StackHists funtion.
     )
     argParser.add_argument('--cut',            action='store',                    type=str,            default='jet30',          help="Which selection?" )
     
