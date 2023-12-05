9783c177580be1e4dcdd189d33a518a1796ddb15
RN <rabnora@example.com>
2023-12-05 23:17:27 +0100

    updated files from master

--- CutFlowHistScript.py
+++ CutFlowHistScript.py
@@ -5,24 +5,34 @@ import types
 sys.path.append('../')
 from Sample.SampleChain import SampleChain
 from Sample.Dir import plotDir
-from Sample.FileList_2016 import samples as samples_2016
+from Sample.FileList_UL2016 import samples as samples_2016
+from Sample.FileList_UL2016PostVFP import samples as samples_2016Post
+from Sample.FileList_UL2016PreVFP import samples as samples_2016Pre
+from Sample.FileList_UL2017 import samples as samples_2017
+from Sample.FileList_UL2018 import samples as samples_2018
+
 
 #samplesRun = ['WJetsToLNu', 'TTSingleLep_pow', 'MET_Data',]
-samplesRun = ['MET_Data',]
+samplesRun = ['QCD', 'ZJetsToNuNu']
 fileperjobMC = 5
 fileperjobData = fileperjobMC
 TotJobs = 4
-year = 2016
+year = 2016PostVFP
 
 txtline = []
 
-if year==2016:
+if year=='2016':
     samplelist = samples_2016
-elif year==2017:
+elif year=='2016PreVFP':
+    samplelist = samples_2016Pre
+elif year=='2016PostVFP':
+    samplelist = samples_2016Post
+elif year=='2017':
     samplelist = samples_2017
 else:
     samplelist = samples_2018
-
+            
+    
 for sL in samplesRun:
     if isinstance(samplelist[sL][0], types.ListType):
         for s in samplelist[sL]:
@@ -30,12 +40,12 @@ for sL in samplesRun:
             fileperjob = fileperjobData if ('Run' in sample or 'Data' in sample) else fileperjobMC
             tfiles = len(SampleChain.getfilelist(samplelist[sample][0]))
             for i in range(0, tfiles, fileperjobMC):
-                txtline.append("python CutFlowHist.py --sample %s --startfile %i --nfiles %i\n"%(sample, i, fileperjobMC))
+                txtline.append("python CutFlowHist.py --sample %s --year %s --startfile %i --nfiles %i\n"%(sample, year, i, fileperjobMC))
     else:
         tfiles = len(SampleChain.getfilelist(samplelist[sL][0]))
         fileperjob = fileperjobData if ('Run' in sL or 'Data' in sL) else fileperjobMC
         for i in range(0, tfiles, fileperjobMC):
-            txtline.append("python CutFlowHist.py --sample %s --startfile %i --nfiles %i\n"%(sL, i, fileperjobMC))
+            txtline.append("python CutFlowHist.py --sample %s --year %s --startfile %i --nfiles %i\n"%(sL, year, i, fileperjobMC))
                 
 fout = open("parallelJobsubmit.txt", "w")
 fout.write(''.join(txtline))
